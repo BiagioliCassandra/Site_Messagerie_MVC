10282814a58e389fde1e0175ad1e60bdf2eb1ae2
cassi <biagioli.cassandra@gmail.com>
2018-12-04 20:30:04 +0100

    create and finish add and delete function

--- view/volunteersView.php
+++ view/volunteersView.php
@@ -2,8 +2,10 @@
 include("template/header.php");
 ?>
 <section>
-    <h2>Liste des bénévoles</h2>
+    <h2 class="text-center">Liste des bénévoles</h2>
     <ul class="list-group list-group-flush">
+    <a href="index.php?&action=volunteerAddView">Ajouter un bénévole</a>
+    <a href="index.php?&action=viewActions">Voir les évènements</a>
     <?php 
     foreach($volunteers as $key => $volunteer) {
     ?>
@@ -14,6 +16,8 @@ include("template/header.php");
         . $volunteer["availability"] . "<br>"
         . $volunteer["comment"];
         ?>
+        <a href="model/volunteerAddView.php?id=<?php echo $volunteer["id"]; ?>&action=add">Modifier</a>
+        <a href="<?php deleteVolunteer($db, $volunteer["id"]); ?>">Supprimer</a>
         </li>
     </ul>
     <?php 
